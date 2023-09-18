52ef45a2ca0adbfced03ca1abe282b5c5704c55e
Pranit <pranit7cr7@gmail.com>
2023-09-18 15:31:54 +0530

    removed config

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -24,28 +24,7 @@ export default function RootNavigation() {
     }
   );
 
-  // useEffect(() => {
-  //   // Function to retrieve the stored UID from AsyncStorage
-  //   const getStoredUID = async () => {
-  //     try {
-  //       const uidObjectStr = await AsyncStorage.getItem('UID');
-  //       // console.log("root",uidObjectStr)
-  //       if (uidObjectStr) {
-  //         const uidObject = JSON.parse(uidObjectStr);
-  //         setUserUid(uidObject.uid);
-  //       } else {
-  //         setUserUid(null);
-  //       }
-  //     } catch (error) {
-  //       console.error("Error while retrieving UID from AsyncStorage:", error);
-  //       // Handle the error as needed (e.g., show an error screen)
-  //       setUserUid(null);
-  //     }
-  //   };
 
-  //   // Call the function to retrieve the stored UID
-  //   getStoredUID();
-  // }, [userUid]);
 
   // Render the appropriate stack based on the presence of the UID
   return userUid ? <SignedInStack /> : <SignedOutStack />;
